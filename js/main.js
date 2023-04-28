52167f7f8488f226906affe47b481220c1dd3234
paulwanghr <whrgxl8687@126.com>
2023-04-28 15:36:44 +0800

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Hongcai Deng on 2015/12/28.
+ */
+
+'use strict';
+
+let path = require('path');
+let mailin = require('node-mailin');
+let config = require(path.join(__dirname, '..', 'config-default.json'));
+let logger = require('tracer').console()
+
+mailin.start(config.mailin);
+
+mailin.on('error', function(err) {
+  console.error(err.stack);
+});
+
+mailin.on('message', function (connection, data, content) {
+  logger.info("FROM:" + data.envelopeFrom.address + "  TO:"+ data.envelopeTo[0].address);
+});
+
+module.exports = mailin;
